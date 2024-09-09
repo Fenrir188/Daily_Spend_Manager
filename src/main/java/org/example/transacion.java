45e58edbba19cb513a7ec32de4f12b3c34cc0b3a
Juan olaya <olayaherrerajuanca@gmail.com>
2024-09-08 22:26:29 -0500

    creacion de clases con sus atributos

---
+++ src/main/java/org/example/transacion.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Date;
+
+public class transacion {
+
+    double monto;
+    String gasto, ingresos, descripcion;
+    Date fecha;
+
+}
---
+++ src/main/java/org/example/usuario.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class usuario {
+
+    String usuario,correo_electronico,telefono,contrasena;
+    int id,edad;
+}
